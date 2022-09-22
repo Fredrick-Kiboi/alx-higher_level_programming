083dfa1a7985c66d2881196eaf7bc9a0923ca1ab
Fredrick-Kiboi <fredrickiboi@gmail.com>
2022-09-22 09:25:40 -0700

    2-args.py correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    import sys
+    length = len(sys.argv) - 1
+
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
+
+    if length >= 1:
+        length = 0
+        for arguments in sys.argv:
+            if length != 0:
+                print("{}: {}".format(length, arguments))
+            length += 1
