8719f501e1e6dd72fd7f75dc6c58849ee5e26306
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-05-17 14:32:04 +0000

    update a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that replaces or adds key/value in a dictionary
+
+Args:
+    A dictionary, key, value
+
+Returns: 
+    The updated dictionary
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
