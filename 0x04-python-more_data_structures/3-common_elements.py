8bf1372b52d58254defaf9e8f1f382a1ec2cafb1
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-05-17 14:18:35 +0000

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+functin that returns a set of common elements in two sets
+
+Args:
+    set_1: first set
+    set_2: second set
+
+Returns:
+    the common elements
+"""
 
 
 def common_elements(set_1, set_2):
