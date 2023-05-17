9db8e697e06d8817c73c51674d1f45a87fcd1ea6
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-05-17 11:59:06 +0000

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Function that replaces all occurences of an element
+by another in a new list
+
+Args:
+    my_list: initial list
+    search: element to replace in the list
+    replace: new element
+
+Returns:
+    new_list
+"""
+
+
+def search_replace(my_list, search, replace):
+  new_list = my_list[:]
+  for i in range(len(new_list)):
+      if new_list[i] == search:
+          new_list[i] = replace
+  return (new_list)
