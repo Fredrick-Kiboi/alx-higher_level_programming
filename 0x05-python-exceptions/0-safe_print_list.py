a3319e64328f53aa8a5df8e74d81d5747f6d7464
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-05-31 21:14:14 +0300

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@
 
 Prototype:
     def safe_print_list(my_list=[], x=0):
-    
-Args:
+
+    Args:
     my_list (list): the list containing the elements
     x (int): number of elements to be printed
 
@@ -13,18 +13,15 @@ Returns:
     int: The actual number of elements printed
 """
 
+
 def safe_print_list(my_list=[], x=0):
     """
     Prints x elements of a list
 
-    The function attempts to print the specified number of elements from the given list. If the length of the list
-    is less than x, it will print as many elements as possible and return the actual count. It uses a try-except block
-    for error handling to ensure that the program continues execution even if an exception occurs.
-    
     Args:
         my_list (list): The list containing the elements.
         x (int): The number of elements to be printed.
-        
+
     Returns:
         int: The actual number of elements printed.
     """
@@ -33,7 +30,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(f"{my_list[i]}", end="")
             count += 1
-    except:
+    except Exception:
         pass
     finally:
         print()
