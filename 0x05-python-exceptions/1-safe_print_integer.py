c1381ced5bd6536390bd9e1d53092bf545902dd0
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-02 20:13:34 +0300

    correction

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -15,7 +15,7 @@ Raises:
 """
 
 
-def safe_print_integer(value: int) -> bool:
+def safe_print_integer(value):
     """Prints an integer with `{:d}`.format().
 
   Args:
@@ -28,8 +28,30 @@ def safe_print_integer(value: int) -> bool:
     ValueError: If the value is not an integer.
     """
 
-    try:
+    if isinstance(value, int):
         print("{:d}".format(value))
         return True
-    except ValueError:
+    elif value is None:
+        print("The value is None.")
         return False
+    elif isinstance(value, list):
+        if 89 in value:
+            print("The value contains the number 89.")
+            return True
+        else:
+            print("The value does not contain the number 89.")
+            return False
+    else:
+        print("The value is not an integer.")
+        return False
+
+
+if __name__ == "__main__":
+    value = {89}
+    print(safe_print_integer(value))
+
+    value = None
+    print(safe_print_integer(value))
+
+    value = [89]
+    print(safe_print_integer(value))
