39ad6d63a4296ae88b5e7638163cad0f1b1af178
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-02 19:51:31 +0300

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Function that prints an integer with "{:d}".format()
+
+Prototype: def safe_print_integer(value):
+
+Args:
+    value : type to be evaluated if it's an integer
+
+Returns:
+     True : if it is an integer
+     False : Otherwise
+"""
+
+def safe_print_integer(value):
+    """
+Function that prints an integer with "{:d}".format()
+
+Prototype: def safe_print_integer(value):
+
+Args:
+    value : type to be evaluated if it's an integer
+
+Returns:
+     True : if it is an integer
+     False : Otherwise
+"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
