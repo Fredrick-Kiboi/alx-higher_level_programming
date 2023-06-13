a2980f442e4b87e27536c94ea57d0cc9e5d67409
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-13 13:57:22 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
+"""defining read_file function"""
 
-"""
-read_file - function that reads a text file (UTF-8) and prints to stdout
-
-Args:
-    filename (str): the file to read from
-
-Returns:
-    None
-"""
 
 def read_file(filename=""):
-    """
-    Reads the contents of the specified text file and prints it to stdout.
-    """
-    with open(filename, mode="r", encoding="utf-8") as myfile:
-        print(myfile.read())
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
