1854de0bc4415f0fe30ca8888f29430e8a13ca0a
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-13 15:17:33 +0300

    to JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-to_json_string - function that returns the JSON representation of an object(string)
+to_json_string - function that returns the JSON
+representation of an object(string)
 
 Args:
     my_obj (string): the object to return the JSON rep of
@@ -8,6 +9,7 @@ Args:
 Returns:
     None
 """
+
 
 import json
 
