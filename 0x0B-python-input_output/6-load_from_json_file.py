ae67ac9034e05b07cc8f842c8b9260d2414b5731
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-13 20:03:56 +0300

    load from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -18,5 +18,7 @@ def load_from_json_file(filename):
     """
     creates an Object from a "JSON file"
     """
-    with open(filename, mode="r", encoding="utf-8") as file:
-        return json.loads(file)
+    with open(filename, 'r') as file:
+        json_data = file.read()
+        obj = json.loads(json_data)
+        return obj
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Script that adds all argurments to a python list,
+and then saves them to a file
+
+File must be saved as a JSON representation in a file named
+add_item.json
+"""
