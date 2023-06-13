d4d584449a3e03278bda0c749c7feddf0d2f671c
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-13 18:32:10 +0300

    load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+load_from_json_file - function that creates an Object
+from a "JSON file"
+
+Args:
+    filename : The "JSON" filename
+
+Returns:
+    None
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a "JSON file"
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        return json.loads(file)
