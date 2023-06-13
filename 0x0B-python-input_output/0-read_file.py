2b478a109dcdeaa1b3e6fdb3bb04832d0a222be3
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-13 13:48:45 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+read_file - function that reads a text file (UTF-8) and prints to stdout
+
+Args:
+    filename (str): the file to read from
+
+Returns:
+    None
+"""
 
 
 def read_file(filename=""):
