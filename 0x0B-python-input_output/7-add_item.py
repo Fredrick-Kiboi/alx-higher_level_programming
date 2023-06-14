9ca81a0af272a7d6d1f03cca761176d09d433039
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-14 05:17:07 +0300

    load, add & save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""module to load and save"""
+"""Module to load and save"""
 import sys
+from typing import List
+import json
+
+def save_to_json_file(my_obj: List[str], filename: str) -> None:
+    """Saves a Python object to a JSON file"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename: str) -> List[str]:
+    """Loads a Python object from a JSON file"""
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
-
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
