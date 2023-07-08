46d92bca7fc21bb3d9eeb1b0c6bdcb06d5f24ab6
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-08 19:41:10 +0300

    getter & setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A class Square
+"""
+
+
+class Square:
+    """
+    A class square that defines a square by:
+    - Private instance attribute : size
+    - Instantiation with optional size: def __init__(self, size=0):
+    - size must be an integer, otherwise raise a TypeError
+    - if size is less than 0, raise a ValueError
+    """
+    def __init__(self, size=0):
+        """
+        Initialization function
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets what the value of the attribute is"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Sets the value of the attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area
+        """
+        return self.__size * self.__size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -77,8 +77,10 @@
 *mandatory*
 - Write a class Square that defines a square by: (based on 1-square.py)<br>
   - Private instance attribute: size<br>
-  - Instantiation with optional size: def __init__(self, size=0):<br>
-  - size must be an integer, otherwise raise a TypeError exception with the message size must be an integer<br>
-  - if size is less than 0, raise a ValueError exception with the message size must be >= 0<br>
+    - property def size(self): to retrieve it
+    - property setter def size(self, value): to set it:
+    - size must be an integer, otherwise raise a    TypeError exception with the message size must be an integer<br>
+    - if size is less than 0, raise a ValueError exception with the message size must be >= 0<br>
+- - Instantiation with optional size: def __init__(self, size=0):<br>
 - You are not allowed to import any module
 - Public instance method: def area(self): that returns the current square area
