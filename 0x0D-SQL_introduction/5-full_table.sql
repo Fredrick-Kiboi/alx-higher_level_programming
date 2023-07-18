a25117f76088594fcccf7d5a51cf2594d130f4ee
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-18 14:54:56 +0000

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- script that prints the full description of the table first_table
 -- the database name will be passed as an argument
 -- you are not allowed to use DESCRIBE or EXPLAIN
-USE hbtn_0c_0;
 SHOW CREATE TABLE first_table;
