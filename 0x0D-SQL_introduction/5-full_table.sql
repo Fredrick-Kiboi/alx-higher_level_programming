2b95b137661ff84c1251a6f4d8359b83d976c914
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-18 12:06:54 +0000

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- script that prints the full description of the table first_table
+-- the database name will be passed as an argument
+-- you are not allowed to use DESCRIBE or EXPLAIN
+SHOW COLUMNS FROM hbtn_0c_0.first_table
+
