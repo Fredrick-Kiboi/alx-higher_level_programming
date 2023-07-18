699e643a8699bf6d4c27c9ca5498bd2a5a8e10ac
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-18 12:23:01 +0000

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- script that prints the full description of the table first_table
 -- the database name will be passed as an argument
 -- you are not allowed to use DESCRIBE or EXPLAIN
-USE INFORMATION_SCHEMA;
-SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_TYPE FROM COLUMNS WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW COLUMNS FROM first_table
