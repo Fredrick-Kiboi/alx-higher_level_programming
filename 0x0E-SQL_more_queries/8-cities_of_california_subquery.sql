c457141df3ae357a3af1e59f658199f684e4f020
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 12:21:49 +0000

    Cities of california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,4 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use JOIN
 USE hbtn_0d_usa;
-SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC;
-
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,5 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results should be in ascending order by cities.id
 -- You can use only one SELECT statement
-
+use hbtn_0d_usa;
+SELECT 
