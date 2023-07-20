aefa24c145633e32f2aeee7cf98d269605661a0d
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 09:33:54 +0000

    Cities table

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- Each record should display: tv_shows.title - tv_shows_genres.genre_id
+-- results should be in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- you can use only SELECT statement
+
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- script that lists all cities in database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- Results should be in ascending order by cities.id
+-- You can use only one SELECT statement
+
