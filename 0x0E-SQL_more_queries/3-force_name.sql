192c35f68369fc4740fc7a58cacd54ba8be693e2
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 08:34:03 +0000

    create the table force_name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,6 +2,5 @@
 -- force name description: id INT, name VARCHAR(256) can't be null
 -- the database name will be passed an argument
 -- if the table exists, script shouldn't fail
-USE hbtn_0d_2;
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
 
