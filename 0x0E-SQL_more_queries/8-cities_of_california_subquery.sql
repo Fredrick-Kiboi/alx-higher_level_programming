36080d26236f298989fe030561c25e72aa8c1cd6
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 12:05:48 +0000

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use JOIN
 USE hbtn_0d_usa;
-SELECT * FROM cities WHERE state_id = 1 ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC;
 
