6b225294f24e4fb8ae4489150a286c80f2c41be2
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 20:07:03 +0000

    genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,9 @@
   -- results should be in ascending order by tv_shows.title and tv_show_genres.genre_id
   -- you can use only SELECT statement
 USE hbtn_0d_tvshows;
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_genres
+JOIN tv_show_genres JOIN tv_shows
+ON tv_shows.id = show_id AND tv_genres.id = genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
