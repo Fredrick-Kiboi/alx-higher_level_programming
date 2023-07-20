77e46f26e56b607b6d97567b6d77973bb2258805
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 07:33:03 +0000

    Create User

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,5 @@
 -- the user password should be set to user_0d_1_pwd
 -- If user already exist, you script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
