3562c9f191c020289f2817656e987dfd6a9b4387
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-20 16:59:16 +0000

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,7 +4,7 @@
   -- You can use only one SELECT statement
 use hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states
-ON cities.state_id = states.id
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
 
