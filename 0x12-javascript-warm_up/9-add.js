c1bcc6728b63881e7f75775368c665ec8231984f
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-08-15 02:15:28 +0000

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-function add (a, b) {
+function add(a, b) {
   return a + b;
 }
-const x = process.argv[2];
-const y = process.argv[3];
-console.log(add(x, y));
+
+let arg = process.argv.length - 2;
+if (arg === 0 || arg === 1) {
+	console.log('NaN')
+} else {
+	let x = Number(process.argv[2]);
+	let y = Number(process.argv[3]);
+	sum = add(x, y);
+	console.log(sum);
+}
